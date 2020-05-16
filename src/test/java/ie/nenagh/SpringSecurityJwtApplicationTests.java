80831b5c94c8a56694cc71a1e3e19f9d35b2cce6
sorabh <guptasorabh51@gmail.com>
2020-05-16 13:03:08 +0100

    first commi

---
+++ src/test/java/ie/nenagh/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.nenagh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
